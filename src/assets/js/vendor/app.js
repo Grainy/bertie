5847cc5c6a0c756030fb0e5b607cde5d8d63fd38
Steven <stevengrain@hotmail.com>
2013-11-10 23:48:55 +0000

    added content boxes

--- src/assets/js/vendor/app.js
+++ src/assets/js/vendor/app.js
@@ -7,21 +7,18 @@ if (isMobile) {
 	touchType = 'click';
 };
 
-console.log(touchType);
-
 var app = (function () {
 
 	function _slideControl () {
 		$('.slide').on(touchType, function() {
 			if (disableMouse === false) {
 				disableMouse = true;
-				if ($(this).hasClass('active')) {
-					$(this).removeClass('active');
-					$('.slide').removeClass('inactive');
-				} else {
-					$('.slide').removeClass('active').addClass('inactive');
-					$(this).addClass('active');
-				};
+
+				$('.slide').removeClass('active').addClass('inactive');
+				$('.content').removeClass('active').css('transition-delay', '0');
+				$(this).addClass('active');
+				$(this).children('.content').addClass('active').css('transition-delay', '1s');;
+
 				setTimeout(function() {
 					disableMouse = false;
 				}, 1000);
